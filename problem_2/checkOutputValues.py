93d539c4209ed07980446563325f706f043b0f0f
leggat <leggat@cern.ch>
2023-05-19 13:04:21 +0100

    A short script to identify problematic files for problem 2

---
+++ problem_2/checkOutputValues.py
@@ -0,0 +1,17 @@
+import os
+
+#Get a list of the valuex files in this directory
+files = [f for f in os.listdir(".") if f.startswith("value")]
+
+#Now open each file
+for fileName in files:
+    #Open this file
+    f = open(fileName,"r")
+    #Get the data values from the file
+    dataValues = f.readline().split("\t")
+    #Loop through the data values and check that each one contains a valid entry
+    for i in range(len(dataValues)):
+        if not dataValues[i]: print "File {0} is missing entry value {1}".format(fileName,i)
+    #If there are less than 100 entries in dataValues, report as an issue
+    if len(dataValues) < 100: print "{0} has fewer than 100 data entries!".format(fileName)
+
