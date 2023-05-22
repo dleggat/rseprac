1604cfade5e67d33322598879e9986e21c6355c2
leggat <leggat@cern.ch>
2023-05-22 12:53:13 +0100

    Moved checkOutputs to a separate method

--- problem_2/checkOutputValues.py
+++ problem_2/checkOutputValues.py
@@ -1,11 +1,8 @@
 import os
 
-#Get a list of the valuex files in this directory
-files = [f for f in os.listdir(".") if f.startswith("value")]
-
-#Now check each file
-for fileName in files:
-    #Open this file
+#Check a file for missing 
+def checkOutputs(fileName):
+    #Open the file
     f = open(fileName,"r")
     #Get the data values from the file
     dataValues = f.readline().split("\t")
@@ -14,4 +11,12 @@ for fileName in files:
         if not dataValues[i]: print "File {0} is missing entry value {1}".format(fileName,i)
     #If there are less than 100 entries in dataValues, report as an issue
     if len(dataValues) < 100: print "{0} has fewer than 100 data entries!".format(fileName)
+    f.close()
+
+#Get a list of the valuex files in this directory
+files = [f for f in os.listdir(".") if f.startswith("value")]
+
+#Now check each file
+for fileInDir in files:
+    checkOutputs(fileInDir)
 
